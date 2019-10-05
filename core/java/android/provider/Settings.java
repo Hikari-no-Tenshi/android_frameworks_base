556d8f4fcbc2b9dc88b6fe88b97aca18ce66e106
maxwen <max.weninger@gmail.com>
2019-10-05 11:44:53 +0200

    base: SystemUI: add separate config for pulse brightness [1/2]
    config_screenBrightnessPulse
    if value is -1 it is usiing config_screenBrightnessDoze
    
    Change-Id: I32771db0726d7ad5d12dacef0623e1fd720060e8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5760,6 +5760,18 @@ public final class Settings {
         public static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
 
         /**
+         * User definable value of pulse notification screen brightness
+         * @hide
+         */
+        public static final String PULSE_BRIGHTNESS = "pulse_brightness";
+
+        /**
+         * User definable value of aod screen brightness
+         * @hide
+         */
+        public static final String DOZE_BRIGHTNESS = "doze_brightness";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
