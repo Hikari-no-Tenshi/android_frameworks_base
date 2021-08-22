bb02c95f4b6534d5975cca3ffa14ddfc23a37a27
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-08-22 17:06:53 +0300

    FODCircleView: Update animation position on circle showing when dreaming

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -546,6 +546,9 @@ public class FODCircleView extends ImageView implements TunerService.Tunable, Co
         });
 
         if (mFODAnimation != null && mIsRecognizingAnimEnabled) {
+            if (mIsDreaming) {
+                updatePosition();
+            }
             mHandler.post(() -> mFODAnimation.showFODanimation());
         }
 
@@ -671,7 +674,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable, Co
         mPressedParams.x = mParams.x = x;
         mPressedParams.y = mParams.y = y;
 
-        if (mIsDreaming) {
+        if (mIsDreaming && !mIsCircleShowing) {
             mParams.x += mDreamingOffsetX;
             mParams.y += mDreamingOffsetY;
         }
