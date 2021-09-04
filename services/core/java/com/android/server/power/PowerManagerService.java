Project-Xtended/frameworks_base

PowerManagerService: Send broadcast when going to sleep

services/core/java/com/android/server/power/PowerManagerService.java
@@ -1945,7 +1945,15 @@ public final class PowerManagerService extends SystemService
         } finally {
             Trace.traceEnd(Trace.TRACE_TAG_POWER);
         }
+        mHandler.post(() -> sendSleepBroadcast());
         return true;
+    }
+
+    private void sendSleepBroadcast() {
+        Intent intent = new Intent(
+                com.android.internal.util.xtended.content.Intent.ACTION_GO_TO_SLEEP);
+        intent.setFlags(Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
+        mContext.sendBroadcastAsUser(intent, UserHandle.SYSTEM);
     }
 
     private void napInternal(long eventTime, int uid) {

Hikari-no-Tenshi/android_frameworks_base

services/core/java/com/android/server/power/PowerManagerService.java
@@ -1972,7 +1972,15 @@ public final class PowerManagerService extends SystemService
         } finally {
             Trace.traceEnd(Trace.TRACE_TAG_POWER);
         }
+        mHandler.post(() -> sendSleepBroadcast());
         return true;
+    }
+
+    private void sendSleepBroadcast() {
+        Intent intent = new Intent(
+                com.android.internal.util.crdroid.content.Intent.ACTION_GO_TO_SLEEP);
+        intent.setFlags(Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
+        mContext.sendBroadcastAsUser(intent, UserHandle.SYSTEM);
     }
 
     private void napInternal(long eventTime, int uid) {